c3c0535edd4a46711c05ebe9c53ecffd912e2fd4
alexxx113 <lead1100@ya.ru>
2026-06-01 02:38:51 +0300

    Update run.sh

--- app/run.sh
+++ app/run.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 set -e
 
-# Читаем параметры из стандартного файла аддонов HA
+# Читаем настройки из стандартного файла аддонов Home Assistant
 if [ -f /data/options.json ]; then
     ROOT_PASSWORD=$(jq --raw-output '.root_password' /data/options.json)
     WEB_PORT=$(jq --raw-output '.web_port' /data/options.json)
@@ -13,13 +13,17 @@ fi
 
 mkdir -p /data/config /data/plugins
 
+# Копируем пример конфига, если пользовательского ещё нет
 if [ ! -f /data/config/init.conf ]; then
     cp /app/config.default/init.conf /data/config/init.conf
 fi
 
+# Меняем пароль и порт в конфиге
 sed -i "s/^password:.*/password: ${ROOT_PASSWORD}/" /data/config/init.conf
 sed -i "s/^port:.*/port: ${WEB_PORT}/" /data/config/init.conf
 
+# Записываем пароль в отдельный файл (как в оригинальной инструкции)
 echo -n "$ROOT_PASSWORD" > /data/config/passwd
 
+# Запускаем lampac
 exec /app/lampac -config /data/config/init.conf -plugins /data/plugins
