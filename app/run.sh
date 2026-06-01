d70dd37964fa8306d10eb1922184da49816361de
alexxx113 <lead1100@ya.ru>
2026-06-01 14:45:55 +0300

    Add files via upload

--- app/run.sh
+++ app/run.sh
@@ -1,13 +1,2 @@
-#!/bin/sh
-
-CONFIG_DIR=/data/config
-mkdir -p $CONFIG_DIR
-
-if [ ! -f $CONFIG_DIR/init.conf ]; then
-    cp /app/config/example.init.conf $CONFIG_DIR/init.conf
-fi
-
-ROOT_PASSWORD=$(jq -r '.root_password' /data/options.json 2>/dev/null || echo "changeme")
-sed -i "s/^password:.*/password: $ROOT_PASSWORD/" $CONFIG_DIR/init.conf
-
-exec dotnet /app/Lampac.dll --config=$CONFIG_DIR/init.conf
+echo "start 8000 web"
+cat /home/passwd
